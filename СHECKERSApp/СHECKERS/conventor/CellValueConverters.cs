0ce8dea1fe593b5608c8e099315c30361e267e25
Кукіль Костянтин Павлович <ipz241_kkp@student.ztu.edu.ua>
2026-04-24 17:25:05 +0300

    Base concept game Commit

---
+++ СHECKERSApp/СHECKERS/conventor/CellValueConverters.cs
@@ -0,0 +1,41 @@
+﻿using СHECKERS.Models;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace СHECKERS.Conventor
+{
+    public class CellValueToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CellValueEnum cellValue)
+            {
+                return cellValue switch
+                {
+                    CellValueEnum.WhiteChecker => Brushes.White,
+                    CellValueEnum.BlackChecker => Brushes.Black,
+                    _ => Brushes.Transparent
+                };
+            }
+            return Brushes.Transparent;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => null;
+    }
+
+    public class CellValueToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CellValueEnum cellValue && cellValue != CellValueEnum.Empty)
+                return System.Windows.Visibility.Visible;
+            return System.Windows.Visibility.Collapsed;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => null;
+    }
+}
---
+++ СHECKERSApp/СHECKERS/models/Cell.cs
@@ -0,0 +1,71 @@
+﻿using СHECKERS.ViewModels.Base;
+
+namespace СHECKERS.Models
+{
+    public class Cell : ViewModel
+    {
+        private CellValueEnum cellValueEnum;
+        private bool act;
+        private int row;
+        private int column;
+        private Board? board;
+
+        public CellValueEnum Cellvalueenum
+        {
+            get => cellValueEnum;
+            set
+            {
+                cellValueEnum = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Act
+        {
+            get => act;
+            set
+            {
+                act = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Row
+        {
+            get => row;
+            set
+            {
+                row = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Column
+        {
+            get => column;
+            set
+            {
+                column = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Board? Board
+        {
+            get => board;
+            set
+            {
+                board = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Cell(int row, int column, Board board)
+        {
+            Row = row;
+            Column = column;
+            Board = board;
+            cellValueEnum = CellValueEnum.Empty;
+        }
+    }
+}
